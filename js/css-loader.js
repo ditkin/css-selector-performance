468354bcddb06ee82704dd0748b3aac64b886609
David Itkin <ditkin@constantcontact.com>
2016-12-10 12:02:00 -0500

    Adjust spacing

--- js/css-loader.js
+++ js/css-loader.js
@@ -1,20 +1,20 @@
 var input = document.getElementById('css-picker');
 input.addEventListener('keyup', function (e) {
-	if (e.keyCode == 13) {
-		loadCss(e.currentTarget.value);
-	}
+  if (e.keyCode == 13) {
+    loadCss(e.currentTarget.value);
+  }
 });
 
 function loadCss(sheetName) {
-	var head = document.getElementsByTagName('head')[0];
-	var cssNode = document.createElement('link');
+  var head = document.getElementsByTagName('head')[0];
+  var cssNode = document.createElement('link');
 
-	var url = 'css/' + sheetName + '.css';
-	cssNode.href = url;
+  var url = 'css/' + sheetName + '.css';
+  cssNode.href = url;
   cssNode.type = 'text/css';
-	cssNode.rel = 'stylesheet';
+  cssNode.rel = 'stylesheet';
 
-	cssNode.onload = timeThis;
+  cssNode.onload = timeThis;
 
-	head.appendChild(cssNode);
+  head.appendChild(cssNode);
 }
